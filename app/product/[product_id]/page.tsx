82f412da3e7814e0fe52eb7929e48ab9f867dc9d
mely-h <melissa.hanouti@edu.devinci.fr>
2025-04-03 14:22:07 +0200

    Ameliore style

--- app/product/[product_id]/page.tsx
+++ app/product/[product_id]/page.tsx
@@ -3,7 +3,7 @@
 import Gallery from "@/components/common/product/gallery";
 import { ProductDescription } from "@/components/common/product/product-description";
 import { headers } from 'next/headers';
-
+import Link from "next/link"
 export default async function Product({params}: { params: Promise<{ product_id: string }> }) {
 
   const { product_id } = await params;
@@ -26,19 +26,46 @@ export default async function Product({params}: { params: Promise<{ product_id: 
   }
 
   return (
-    <div className="flex gap-6">
-      <div className="w-[70%]">
+    <>
+    <div className="fixed top-0 right-0 h-screen w-[50vw]">
+      <img
+        src="/images/gradient.png"
+        alt="Background gradient"
+        className="fixed h-full w-full object-cover"
+      />
+    </div>
+
+  
+  <div className="relative z-10 bg-white min-h-screen px-6 py-8 lg:px-16">
+  
+    <Link href="/">
+      <button variant="outline" className="mb-8">
+        ← Retour
+      </button>
+    </Link>
+
+ 
+    <div className="flex flex-col lg:flex-row items-center ">
+    
+      <div className="w-full lg:w-1/2">
         <Gallery images={product.images} />
       </div>
-      <div className="w-[30%] shrink-0">
-        <ProductDescription
-          stock={2}
-          title={product.name}
-          amount={product.price}
-          productDescription={product.product_description}
-          causeDescription={product.animal_decription}
-        />
+      <div className="w-full lg:w-1/2 flex items-center justify-center">
+        <div className="w-full max-w-md">
+          <ProductDescription
+            stock={2}
+            title={product.name}
+            amount={product.price}
+            productDescription={product.product_description}
+            causeDescription={product.animal_decription}
+          />
+        </div>
       </div>
     </div>
-  );
+    </div>
+   
+ </>
+);
 }
+
+
